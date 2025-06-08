8fa3a95c11489f8e550a6c9429c4fed435482ff3
VarunMayandi <varunmayandi9194@gmail.com>
2025-06-09 01:26:11 +0530

    small

--- src/api.js
+++ src/api.js
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
+const API_URL = process.env.REACT_APP_API_URL || 'https://inv-back-dbul.onrender.com';
+
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api'
+  baseURL: `${API_URL}/api`,
+  headers: {
+    'Content-Type': 'application/json'
+  }
 });
 
 export default API;
